334ac5dd022a41a8a1e40a9f3c4f58ce8965b954
Hadra 92i <hadraiizi547@gmail.com>
2022-03-08 11:49:51 +0100

    creating router and api routes

---
+++ routes/goalRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { getGoals, setGoal, updateGoal, deleteGoal }
+    = require('../controllers/GoalController');
+
+// router.get('/', getGoals);
+// router.post('/', setGoal);
+// router.put('/:id', updateGoal);
+// router.delete('/:id', deleteGoal);
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
